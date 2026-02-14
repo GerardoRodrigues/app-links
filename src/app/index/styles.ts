46221052d90da696861151ea20b9866808aa7640
Gerardo Rodrigues <gerardofilho56@gmail.com>
2026-02-14 13:48:51 -0300

    header

---
+++ src/app/index/styles.ts
@@ -0,0 +1,24 @@
+import { colors } from "@/src/styles/colors";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 64,
+  },
+  title: {
+    fontSize: 22,
+    color: colors.green[900],
+  },
+  header: {
+    paddingHorizontal: 24,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  logo: {
+    height: 32,
+    width: 38,
+  },
+});
